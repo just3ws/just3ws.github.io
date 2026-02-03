734ca20b88282b5b80b76627c32a6e7bcb2853e0
Mike Hall <mike@just3ws.com>
2026-02-03 02:36:18 -0600

    Validate UGtastic communities and thumbnails

--- bin/validate_ugtastic.rb
+++ bin/validate_ugtastic.rb
@@ -5,11 +5,14 @@ require 'date'
 root = File.expand_path('..', __dir__)
 data_path = File.join(root, '_data', 'ugtastic.yml')
 conf_path = File.join(root, '_data', 'ugtastic_conferences.yml')
+comm_path = File.join(root, '_data', 'ugtastic_communities.yml')
 videos_dir = File.join(root, 'ugtastic', 'videos')
 
 ugtastic = YAML.safe_load(File.read(data_path), permitted_classes: [Time, Date], aliases: true)
 confs = YAML.safe_load(File.read(conf_path), permitted_classes: [Date], aliases: true)['conferences']
+comms = YAML.safe_load(File.read(comm_path), permitted_classes: [Date], aliases: true)['communities']
 conf_slugs = confs.map { |c| c['slug'] }
+comm_slugs = comms.map { |c| c['slug'] }
 
 errors = []
 
@@ -23,6 +26,14 @@ errors = []
 
   if item['conference'] && !conf_slugs.include?(item['conference'])
     errors << "unknown conference slug #{item['conference']} for #{id}"
+  end
+  if item['community'] && !comm_slugs.include?(item['community'])
+    errors << "unknown community slug #{item['community']} for #{id}"
+  end
+
+  if item['thumbnail_local']
+    local = File.join(root, item['thumbnail_local'])
+    errors << "missing local thumbnail for #{id}" unless File.exist?(local)
   end
 
   if id
